37659d3c7388a4a61ff7fa498eb63156c2d36ba8
mTvare6 <austin76912@gmail.com>
2021-05-18 13:45:38 +0530

    (added): don't pull this commit

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ const {app, BrowserWindow, globalShortcut} = require('electron')
 const path = require('path')
 
 function createWindow () {
-    const mainWindow = new BrowserWindow({
-      width: 650,
-      height: 400,
-      center: true,
-      frame: false,
-      resizable:false,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
+  const mainWindow = new BrowserWindow({
+    width: 650,
+    height: 400,
+    center: true,
+    frame: false,
+    //resizable:false,
+    darkTheme: true,
+    title: "htBrowser",
+    focusable: false,
+    alwaysOnTop: true,
+
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
 
   mainWindow.loadFile('index.html')
@@ -20,33 +25,39 @@ function createWindow () {
 
 hidden = false;
 
-  app.whenReady().then(() => {
-    createWindow()
-    app.hide()
-    hidden = true;
+app.whenReady().then(() => {
+  createWindow()
+  app.hide()
+  hidden = true;
 
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-    const ret = globalShortcut.register('CmdOrCtrl+Shift+~', () => {
-      if (!hidden){
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+  const ret = globalShortcut.register('CmdOrCtrl+Shift+~', () => {
+    if (!hidden){
+      if(process.platform==='darwin'){
         app.hide()
-        hidden = true;
       }
       else{
-        app.show()
-        hidden = false;
+        app.quit()
       }
-    })
-    if (!ret) {
-      console.log('registration failed')
+      hidden = true;
     }
-
+    else{
+      app.show()
+      app.focus()
+      hidden = false;
+    }
   })
+  if (!ret) {
+    console.log('registration failed')
+  }
 
-  app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-    // Unregister all shortcuts.
-      globalShortcut.unregisterAll()
-  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+  // Unregister all shortcuts.
+    globalShortcut.unregisterAll()
+})
 
