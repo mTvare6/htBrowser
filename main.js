127d7b2d1dbc15592639d4ad14f21d96b374b7e1
mTvare6 <mTvare6@users.noreply.github.com>
2021-05-18 21:46:20 +0530

    (added): configs

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+const activeWindows = require('electron-active-window');
+
+
+
+
 const {app, BrowserWindow, globalShortcut} = require('electron')
 const path = require('path')
 
 const config =  require((process.platform==="linux")?`${app.getPath("appData")}/htBrowser/config.json`:`${app.getPath("home")}/.config/htBrowser/config.json`)
 
-function createWindow () {
+function createWindow (currentWMClass) {
+
   const mainWindow = new BrowserWindow(
     {
       width: (config.window || {}).width || 640,
@@ -20,17 +26,30 @@ function createWindow () {
       }
     }
   )
+  let urlToLoad = (config.defaultURL || "https://duckduckgo.com");
+
+  for (index in  (config.customURLS || {} )){
+    for (const [wmClass, url] of Object.entries(config.customURLS[index])) {
+      if (wmClass.toLowerCase()==currentWMClass.toLowerCase()){
+        urlToLoad=url
+      }
+    }
+  }
 
 
-  mainWindow.loadURL( config.defaultURL || "https://duckduckgo.com")
+
+  mainWindow.loadURL( urlToLoad )
   return mainWindow;
 }
 
 hidden = false;
 
 app.whenReady().then(() => {
-  let mainWindow = createWindow()
-
+  let mainWindow
+  activeWindows().getActiveWindow().then((result)=>{
+    mainWindow = createWindow(result["windowName"])
+  });
+  
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
